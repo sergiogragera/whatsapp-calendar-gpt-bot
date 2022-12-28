ec1b36d301c0ce844f29d1b3f350c1e7c0229b0b
Sergio Gragera <sergiogragera@gmail.com>
2022-12-28 18:51:46 +0100

    feature: add openai generator

---
+++ textGenerator.js
@@ -0,0 +1,22 @@
+const got = require('got');
+
+async function generate(prompt) {
+  const url = 'https://api.openai.com/v1/completions';
+  const params = {
+    model: "text-davinci-003",
+    prompt,
+    top_p: 1,
+    max_tokens: 200,
+    temperature: 0.6,
+    frequency_penalty: 1.5,
+    presence_penalty: 1.2,
+  };
+  const headers = {
+    'Authorization': `Bearer ${process.env.OPENAI_SECRET_KEY}`,
+  };
+
+  const response = await got.post(url, { json: params, headers: headers }).json();
+  return response.choices[0].text;
+}
+
+module.exports = { generate };
