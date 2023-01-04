245536eb4c694c4d048c673e3463d44be8621217
Sergio Gragera <sergiogragera@gmail.com>
2023-01-04 22:44:38 +0100

    fix: add calendar API scope

--- calendar.js
+++ calendar.js
@@ -4,6 +4,13 @@ const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis')
 
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
