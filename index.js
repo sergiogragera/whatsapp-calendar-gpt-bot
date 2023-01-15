7777e627be05fbb6f46ddb09e1739b898edc5e33
Sergio Gragera <sergiogragera@gmail.com>
2023-01-15 11:05:43 +0100

    feat: add ical option arg

--- index.js
+++ index.js
@@ -6,35 +6,80 @@ const calendar = require('./calendar');
 const generator = require('./textGenerator');
 const { convert } = require('html-to-text');
 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-})
+const commandLineArgs = require('command-line-args')
+const commandLineUsage = require('command-line-usage')
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
+const optionDefinitions = [
+    {
+        name: 'help',
+        alias: 'h',
+        type: Boolean,
+        description: 'Display this usage guide.'
+    },
+    {
+        name: 'ical',
+        type: String,
+        multiple: true,
+        description: 'The iCal files to find events',
+        typeLabel: '<files>'
+    }
+]
 
-client.on('ready', async () => {
-    try {
-        const calendarAuth = await calendar.authorize();
-        const events = await calendar.getEvents(calendarAuth);
-        for (const event of events) {
-            if (event.summary) {
-                console.info(`Event found: ${event.summary}`);
-                const message = convert(event.description) || await generator.generate(event.summary);
-                console.info(`Generated message: ${message}`);
-                for (const attender of event.attendees) {
-                    if (/[0-9]+@[cg].us/.test(attender.email)) {
-                        console.info(`Sending message to ${attender.email}`);
-                        await client.sendMessage(attender.email, message);
-                        console.info(`Message delivered to ${attender.email}`);
+const options = commandLineArgs(optionDefinitions)
+
+if (options.help) {
+    const usage = commandLineUsage([
+        {
+            header: 'Typical Example',
+            content: 'A simple example demonstrating typical usage.'
+        },
+        {
+            header: 'Options',
+            optionList: optionDefinitions
+        },
+        {
+            content: 'Project home: {underline https://github.com/sergiogragera/whatsapp-calendar-gpt-bot}'
+        }
+    ]);
+    console.log(usage);
+} else {
+    const client = new Client({
+        authStrategy: new LocalAuth(),
+    });
+
+    client.on('qr', qr => {
+        qrcode.generate(qr, { small: true });
+    });
+
+    client.on('ready', async () => {
+        try {
+            let events = [];
+            if (options.ical) {
+                console.log(options);
+            }
+            else {
+                const calendarAuth = await calendar.authorize();
+                events = await calendar.getEvents(calendarAuth);
+            }
+
+            for (const event of events) {
+                if (event.summary) {
+                    console.info(`Event found: ${event.summary}`);
+                    const message = convert(event.description) || await generator.generate(event.summary);
+                    console.info(`Generated message: ${message}`);
+                    for (const attender of event.attendees) {
+                        if (/[0-9]+@[cg].us/.test(attender.email)) {
+                            console.info(`Sending message to ${attender.email}`);
+                            await client.sendMessage(attender.email, message);
+                            console.info(`Message delivered to ${attender.email}`);
+                        }
                     }
                 }
             }
+        } catch (err) {
+            console.error(err);
         }
-    } catch (err) {
-        console.error(err);
-    }
-});
+    });
 
-client.initialize();
+    client.initialize();
+}
